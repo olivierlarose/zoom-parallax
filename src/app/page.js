f6dc2b2f3d5c3f1ec181d98da90d35d4aeebf876
Olivier Larose <olivierlarose1@gmail.com>
2024-01-13 15:00:55 -0500

    initial commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,25 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+'use client'
+import styles from './page.module.scss'
+import ZoomParallax from '../components/ZoomParallax/index';
+import { useEffect } from 'react';
+import Lenis from '@studio-freight/lenis'
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+    useEffect( () => {
+        const lenis = new Lenis()
+       
+        function raf(time) {
+            lenis.raf(time)
+            requestAnimationFrame(raf)
+        }
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+        requestAnimationFrame(raf)
+    },[])
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    return (
+        <main className={styles.main}>
+            <ZoomParallax />
+        </main>
+    )
 }
---
+++ src/components/ZoomParallax/index.jsx
@@ -0,0 +1,78 @@
+import styles from './styles.module.scss';
+import Picture1 from '../../../public/images/1.jpeg';
+import Picture2 from '../../../public/images/2.jpeg';
+import Picture3 from '../../../public/images/3.jpg';
+import Picture4 from '../../../public/images/4.jpg'
+import Picture5 from '../../../public/images/5.jpg'
+import Picture6 from '../../../public/images/6.jpg'
+import Picture7 from '../../../public/images/7.jpeg'
+import Image from 'next/image';
+import { useScroll, useTransform, motion} from 'framer-motion';
+import { useRef } from 'react';
+
+export default function Index() {
+    
+    const container = useRef(null);
+    const { scrollYProgress } = useScroll({
+        target: container,
+        offset: ['start start', 'end end']
+    })
+
+    const scale4 = useTransform(scrollYProgress, [0, 1], [1, 4]);
+    const scale5 = useTransform(scrollYProgress, [0, 1], [1, 5]);
+    const scale6 = useTransform(scrollYProgress, [0, 1], [1, 6]);
+    const scale8 = useTransform(scrollYProgress, [0, 1], [1, 8]);
+    const scale9 = useTransform(scrollYProgress, [0, 1], [1, 9]);
+
+    const pictures = [
+        {
+            src: Picture1,
+            scale: scale4
+        },
+        {
+            src: Picture2,
+            scale: scale5
+        },
+        {
+            src: Picture3,
+            scale: scale6
+        },
+        {
+            src: Picture4,
+            scale: scale5
+        },
+        {
+            src: Picture5,
+            scale: scale6
+        },
+        {
+            src: Picture6,
+            scale: scale8
+        },
+        {
+            src: Picture7,
+            scale: scale9
+        }
+    ]
+
+    return (
+        <div ref={container} className={styles.container}>
+            <div className={styles.sticky}>
+                {
+                    pictures.map( ({src, scale}, index) => {
+                        return <motion.div key={index} style={{scale}} className={styles.el}>
+                            <div className={styles.imageContainer}>
+                                <Image
+                                    src={src}
+                                    fill
+                                    alt="image"
+                                    placeholder='blur'
+                                />
+                            </div>
+                        </motion.div>
+                    })
+                }
+            </div>
+        </div>
+    )
+}
